54c5f3adbf3f97413ded7c3b3ea0c289c87ab2e4
Simon <simonkdeveloper@gmail.com>
2019-06-24 09:19:07 -0400

    16:09

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  const [posts,setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   // state= {posts=[]};
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
 
-  useEffect(()=>{
-    const festchPosts = async ()=>{
+  useEffect(() => {
+    const festchPosts = async () => {
       setLoading(true);
       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
@@ -21,16 +22,21 @@ function App() {
 
     festchPosts();
 
-  },[]);
+  }, []);
 
-  console.log(posts);
+  // console.log(posts);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const curretnPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="container mt-5">
-    <div className="App">
-      <h1 className= "text-primary mb-3">App</h1>
-      <Posts posts={posts} loading={loading}/>
-    </div>
+      <div className="App">
+        <h1 className="text-primary mb-3">App</h1>
+        <Posts posts={curretnPosts} loading={loading} />
+        <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+      </div>
     </div>
   );
 }
