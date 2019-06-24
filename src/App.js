623b029c04054cca8f5a28d0598ac4ba75c186cc
unknown <simonkdeveloper@gmail.com>
2019-06-24 07:47:32 -0400

    123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [posts,setPosts] = useState([]);
+  // state= {posts=[]};
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(()=>{
+    const festchPosts = async ()=>{
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    festchPosts();
+
+  },[]);
+
+  console.log(posts);
+
   return (
+    <div className="container mt-5">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className= "text-primary mb-3">App</h1>
+      <Posts posts={posts} loading={loading}/>
+    </div>
     </div>
   );
 }
